6e23f83866f5b0b0f675e95af9db8047beabc6dc
Kyle Walter <walter.kl26@gmail.com>
2021-03-05 21:53:19 -0600

    Update ScfleetsVerificationWorkerApplicationTests.java

--- src/test/java/com/fusionkoding/scfleetsverificationworker/ScfleetsVerificationWorkerApplicationTests.java
+++ src/test/java/com/fusionkoding/scfleetsverificationworker/ScfleetsVerificationWorkerApplicationTests.java
@@ -3,7 +3,6 @@ package com.fusionkoding.scfleetsverificationworker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ScfleetsVerificationWorkerApplicationTests {
 
     @Test
